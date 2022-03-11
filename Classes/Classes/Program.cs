57bac01836d0e7d199fee993fe507fc70dedc1ec
Masenkyo <33390@ma-web.nl>
2022-03-11 10:25:33 +0100

    Gold Classes

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Rekenmachine
+    {
+        static void Main(string[] args)
+        {
+
+            int Number1;
+            int Number2;
+            string Calculation;
+            int Antwoord;
+
+            Console.WriteLine("Calculator");
+            Console.Write("Eerste nummer : ");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+
+            //User input for equation
+            Console.Write("Tweede nummer : ");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type berekening : ( x , : , +, -) ");
+            Calculation = Console.ReadLine();
+
+            if (Calculation == "x")
+            {
+                Antwoord = Number1 * Number2;
+                Console.WriteLine(Number1 + " x " + Number2 + " = " + Antwoord);
+                Console.ReadLine();
+            }
+            else if (Calculation == ":")
+            {
+                Antwoord = Number1 / Number2;
+                Console.WriteLine(Number1 + " / " + Number2 + " = " + Antwoord);
+                Console.ReadLine();
+            }
+            //Getting answers
+            else if (Calculation == "+")
+            {
+                Antwoord = Number1 + Number2;
+                Console.WriteLine(Number1 + " + " + Number2 + " = " + Antwoord);
+                Console.ReadLine();
+            }
+            else if (Calculation == "-")
+            {
+                Antwoord = Number1 - Number2;
+                Console.WriteLine(Number1 + " - " + Number2 + " = " + Antwoord);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Dat kan niet");
+                Console.Write("Type berekening : ( x , : , +, -) ");
+                Calculation = Console.ReadLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
